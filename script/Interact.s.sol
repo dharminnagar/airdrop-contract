39db70ee95459a2d99bdaa2ae334ce3227264308
Dharmin Nagar <dharminnagar@gmail.com>
2024-08-19 13:57:23 +0530

    Airdrop Contract

---
+++ script/Interact.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+    error __ClaimAirdropScript__InvalidSignatureLength();
+
+    address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 CLAIMING_AMOUNT = 25 * 1e18;
+    bytes32 PROOF_1 = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_2 = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes private SIGNATURE =
+        hex"d06839720e7117db254838abc5a116500466bc03dd204248d2f4f027f335dd805879986b90e776f383727a80897987ad2217871509cddde5855ce5f82e3766cc1c";
+
+    bytes32[] proof = [PROOF_1, PROOF_2];
+
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, proof, v, r, s);
+        vm.stopBroadcast();
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert __ClaimAirdropScript__InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        claimAirdrop(mostRecentlyDeployed);
+    }
+}
